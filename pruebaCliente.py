d525693294f4685a9e6d0cfa75389b43a36e318a
foxcarlos <foxcarlos@gmail.com>
2016-06-09 02:01:21 -0430

    trabajando con el cliente trello, se realizaron varias pruebas de captura de datos

--- pruebaCliente.py
+++ pruebaCliente.py
@@ -6,10 +6,106 @@ from trello import TrelloClient
 # token='your-oauth-token-key' - 1316a12594687b611d0c631896b9b421436bd955d0d376162521c5ed267155d8
 # token_secret='your-oauth-token-secret' - 5d0d9ac40b148703315c54e64b7998d2
 
-client = TrelloClient(api_key='05571f7ecf0d9177c020b9ab3495aaac', 
-    api_secret='03a65f30fb4946c03cba7998327f7e10025493f7e036408d6efbcdbd63fc66f5', 
-    token='1316a12594687b611d0c631896b9b421436bd955d0d376162521c5ed267155d8', 
+client = TrelloClient(api_key='05571f7ecf0d9177c020b9ab3495aaac',
+    api_secret='03a65f30fb4946c03cba7998327f7e10025493f7e036408d6efbcdbd63fc66f5',
+    token='1316a12594687b611d0c631896b9b421436bd955d0d376162521c5ed267155d8',
     token_secret='5d0d9ac40b148703315c54e64b7998d2'
 )
 
-print(client.list_boards())
+# Listar los tableros Abiertos
+print( client.list_boards(board_filter="open") )
+
+# Listar todos los tableros
+print( client.list_boards() )
+
+# Listar columnas
+abiertos = client.list_boards(board_filter="open")
+for b in abiertos:
+    b.all_lists()
+
+# Listar atributos
+for b in abiertos:
+    for l in b.all_lists():
+        for c in l.list_cards():
+            print( c.fetch() )
+            
+            
+# Otros Comandos aplicados a las tarjetas
+>>> c.assign
+
+
+>>> c.attach
+
+
+>>> c.attachments
+
+>>> c.attriExp
+
+
+>>> c.board
+<Board trelar>
+
+>>> c.checklists
+
+>>> c.client
+<trello.trelloclient.TrelloClient object at 0xb41b4c4c>
+
+>>> c.closed
+False
+
+>>> c.comment
+<bound method Card.comment of <Card Testing a py-trello, obtener un listado de todos los tableros.>>
+
+
+>>> c.create_date
+datetime.datetime(2016, 6, 9, 4, 49, 2, 620000, tzinfo=tzutc())
+
+>>> c.dateLastActivity
+datetime.datetime(2016, 6, 9, 4, 49, 2, 610000, tzinfo=tzutc())
+
+>>> c.date_last_activity
+datetime.datetime(2016, 6, 9, 4, 49, 2, 610000, tzinfo=tzutc())
+
+>>> c.desc
+u''
+
+>>> c.description
+u''
+
+>>> c.due
+
+>>> c.due_date
+''
+
+>>> c.id
+u'5758f53e74a534e5f6ef252b'
+
+>>> c.idBoard
+u'57581f7d6a945e2f6630a793'
+
+>>> c.idLabels
+[]
+
+>>> c.idList
+u'57582126be36c73d6688b24b'
+
+>>> c.idMembers
+[]
+
+>>> c.idShort
+16
+
+>>> c.labels
+[]
+
+>>> c.label_ids
+[]
+
+>>> c.shortUrl
+u'https://trello.com/c/9bDQNh0q'
+
+>>> c.trello_list
+<List En Desarrollo>
+
+>>> c.url
+u'https://trello.com/c/9bDQNh0q/16-testing-a-py-trello-obtener-un-listado-de-todos-los-tableros'
